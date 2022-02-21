5e31342a66e1301729f69e0b1014c23fa5f11b90
tangc1 <tangci@mgtv.com>
2022-02-21 15:25:26 +0800

    🚧 delete node tools

--- rollup.config.js
+++ rollup.config.js
@@ -11,16 +11,23 @@ export default {
     name: 'funnyUtils',
     file: pkg.browser, // 最终打包出来的文件路径和文件名，这里是在package.json的browser: 'dist/index.js'字段中配置的
     format: 'umd', // umd是兼容amd/cjs/iife的通用打包格式，适合浏览器
-    globals: {
-      'fs': 'fs',
-      'path': 'path',
-      'copy-dir': 'copy-dir',
-      'make-dir': 'make-dir',
-      'rimraf': 'rimraf',
-      'child_process': 'child_process',
-      'os': 'os'
-    }
+    // globals: {
+    //   'fs': 'fs',
+    //   'path': 'path',
+    //   'copy-dir': 'copy-dir',
+    //   'make-dir': 'make-dir',
+    //   'rimraf': 'rimraf',
+    //   'child_process': 'child_process',
+    //   'os': 'os'
+    // }
   },
+  // externals: ['fs',
+  //   'path',
+  //   'copy-dir',
+  //   'make-dir',
+  //   'rimraf',
+  //   'child_process',
+  //   'os'],
   plugins: [ // 打包插件
     resolve(), // 查找和打包node_modules中的第三方模块
     commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
